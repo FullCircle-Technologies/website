b399c4e495aa1125788366a399db04da219b2c18
techakhil-me <techakhilc47@gmail.com>
2024-06-10 17:09:04 +0530

    added input form

--- script.js
+++ script.js
@@ -1,38 +1,139 @@
-// var scene1 = document.getElementById('scene1');
+// var scene1 = document.getElementById("scene1");
 // var parallaxInstance = new Parallax(scene1);
-
 
 // var images = document.querySelectorAll('img');
 // new simpleParallax(images);
 
 
+function showInput(){
+  var input = document.getElementById('input');
+  input.style.display = 'flex';
+  document.getElementById('button').style.display = 'none';
+
+}
+
 // gsap
 gsap.registerPlugin(ScrollTrigger);
 
-let tl = gsap.timeline(
-    {
-        scrollTrigger: {
-            trigger: "section",
-            start: "top top",
-            end: "+=500%",
-            pin: true,
-            scrub: true,
-            // markers: true
-          },
-    }
-);
+let tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: "section",
+    start: "top top",
+    end: "+=300%",
+    pin: true,
+    scrub: true,
+    // markers: true
+  },
+});
 
+// tl.to("#scene1  > img:nth-child(8)", {
+//   height: "600%",
+//   top: "-200%",
+//   // duration: 3,
+//   //   webkitFilter: "blur(10px)",
+//   //   opacity: 0,
+//   ease: "sine.inOut",
+//   duration: 1,
+// }).to(".text1", {
+//   fontSize: "180px",
 
-tl.to("#scene1  > img:nth-child(7)", {
+//   ease: "sine.inOut",
+//   duration: 1,
+// },0)
+//   .to(
+//     "#scene1 > img:nth-child(6)",
+//     {
+//       height: "200%",
+//       top: "-50%",
+//       duration: 3,
+//       //   webkitFilter: "blur(10px)",
+//       //   opacity: 0,
+//       ease: "sine.inOut",
+//       duration: 1.2,
+//     },
+//     0
+//   )
+//   .to(
+//     "#scene1 > img:nth-child(5)",
+//     {
+//       height: "200%",
+//       top: "-50%",
+//       duration: 3,
+//       // webkitFilter: "blur(10px)",
+//       // opacity: 0,
+//       ease: "sine.inOut",
+//       duration: 1.4,
+//     },
+//     0
+//   )
+//   .to(
+//     "#scene1 > img:nth-child(4)",
+//     {
+//       height: "200%",
+//       top: "-50%",
+//       duration: 3,
+//       // webkitFilter: "blur(10px)",
+//       // opacity: 0,
+//       ease: "sine.inOut",
+//       duration: 1.6,
+//     },
+//     0
+//   )
+//   .to(
+//     "#scene1 > img:nth-child(3)",
+//     {
+//       height: "200%",
+//       duration: 3,
+//       top: "-50%",
+//       // webkitFilter: "blur(10px)",
+//       // opacity: 0,
+//       ease: "sine.inOut",
+//       duration: 1.8,
+//     },
+//     0
+//   )
+//   .to(
+//     "#scene1 > img:nth-child(2)",
+//     {
+//       height: "200%",
+//       top: "-50%",
+//       // duration: 3,
+//       // webkitFilter: "blur(10px)",
+//       // opacity: 0,
+//       ease: "sine.inOut",
+//       duration: 2,
+//     },
+//     0
+//   )
+//   .to(
+//     "#scene1 > img:nth-child(1)",
+//     {
+//       height: "120%",
+//       // top: "-5%",
+//       duration: 3,
+//       ease: "sine.inOut",
+//       duration: 3,
+//     },
+//     0
+//   );
+
+tl.to("#img7", {
   scale: 3,
   duration: 3,
 //   webkitFilter: "blur(10px)",
   //   opacity: 0,
   ease: "sine.inOut",
   duration: 1
- 
 })
-.to("#scene1 > img:nth-child(6)", {
+.to("#text1", {
+  scale: 3,
+  duration: 3,
+  webkitFilter: "blur(10px)",
+  opacity: 0,
+  ease: "sine.inOut",
+  duration: 1.2
+},0)
+.to("#img6", {
   scale: 3,
   duration: 3,
 //   webkitFilter: "blur(10px)",
@@ -40,7 +141,7 @@ tl.to("#scene1  > img:nth-child(7)", {
   ease: "sine.inOut",
   duration: 1.2
 },0)
-.to("#scene1 > img:nth-child(5)", {
+.to("#img5", {
     scale: 3,
     duration: 3,
     // webkitFilter: "blur(10px)",
@@ -48,7 +149,7 @@ tl.to("#scene1  > img:nth-child(7)", {
     ease: "sine.inOut",
     duration: 1.4
   },0)
-  .to("#scene1 > img:nth-child(4)", {
+  .to("#img4", {
     scale: 3,
     duration: 3,
     // webkitFilter: "blur(10px)",
@@ -56,7 +157,7 @@ tl.to("#scene1  > img:nth-child(7)", {
     ease: "sine.inOut",
     duration: 1.6
   },0)
-  .to("#scene1 > img:nth-child(3)", {
+  .to("#img3", {
     scale: 3,
     duration: 3,
     // webkitFilter: "blur(10px)",
@@ -64,7 +165,7 @@ tl.to("#scene1  > img:nth-child(7)", {
     ease: "sine.inOut",
     duration: 1.8
   },0)
-  .to("#scene1 > img:nth-child(2)", {
+  .to("#img2", {
     scale: 3,
     duration: 3,
     // webkitFilter: "blur(10px)",
@@ -72,13 +173,18 @@ tl.to("#scene1  > img:nth-child(7)", {
     ease: "sine.inOut",
     duration: 2
   },0)
-  .to("#scene1 > img:nth-child(1)", {
+  .to("#img1", {
     scale: 3,
     duration: 3,
     ease: "sine.inOut",
     duration: 2.2
+  },0)
+  .from("#paper", {
+    top:"200%",
+    zIndex: 10,
+    ease: "sine.inOut",
+    duration: 2.2
   },0);
-
 
 //   gsap.to("#scene1 > img:nth-child(5)", {
 //     scale: 3,
